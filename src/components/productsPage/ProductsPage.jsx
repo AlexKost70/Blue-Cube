d69b20559029677e48d3db0fe92efaedd04ff64f
Alexander Kostrikin <aleksandrkostrikin086@gmail.com>
2023-07-02 14:34:56 +0300

    Fix bugs, improve UX/UI

--- src/components/productsPage/ProductsPage.jsx
+++ src/components/productsPage/ProductsPage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./ProductsPage.css";
-import { Card, CardMedia, CardContent, Typography, Rating, Pagination } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, Rating, Pagination, CircularProgress } from "@mui/material";
 import star from "./star.svg";
 import emptyStar from "./emptyStar.svg";
 
 export default function ProductsPage() {
     const [data, setData] = useState('');
     const [page, setPage] = useState(1);
+    const [isPageLoaded, setIsPageLoaded] = useState(false);
     const handlePageChange = (event, value) => {
         fetchData(value);
+        setIsPageLoaded(false);
         setPage(value);
     }
 
@@ -17,6 +19,7 @@ export default function ProductsPage() {
         fetch('https://skillfactory-task.detmir.team/products?limit=15&page=' + page)
         .then((response) => { return response.json(); })
         .then((serverData) => { setData(serverData.data) })
+        .then(() => setIsPageLoaded(true))
         .catch(() => { console.log('error') });
     }
 
@@ -26,41 +29,52 @@ export default function ProductsPage() {
 
     return(
         <main>
-            {
-                data?.length > 0 &&
-                (data.map((item) => {
-                    return(
-                        <Card sx={{ maxWidth: 250, maxHeight: 358, borderRadius: 4 }} key={item.id}>
-                            <CardMedia
-                                sx={{ height: 250, width: 250 }}
-                                image={item.picture}
-                                title={item.title}
-                            />
-                            <CardContent className="card-content">
-                                <Typography gutterBottom component="div" className="card-title" fontSize={16}>
-                                    {item.title}
-                                </Typography>
-                                <Typography>
-                                    <Rating 
-                                        name="read-only" 
-                                        value={item.rating}
-                                        precision={0.5}
-                                        icon={<img src={star} style={{marginRight: "4px"}} alt="Иконка золотой звезды" />}
-                                        emptyIcon={<img src={emptyStar} style={{marginRight: "4px"}} alt="Иконка белой звезды" />}
-                                        readOnly 
+            { !isPageLoaded ? <div className="loader-block"> <CircularProgress disableShrink /> </div> :
+                <div className="cards-block">
+                    {
+                        data?.length > 0 && data.map((item) => {
+                            return(
+                                <Card sx={{ maxWidth: 250, maxHeight: 358, borderRadius: 4 }} key={item.id}>
+                                    <CardMedia
+                                        sx={{ height: 250, width: 250 }}
+                                        image={item.picture}
+                                        title={item.title}
                                     />
-                                </Typography>
-                                <Typography fontWeight="bold" mt={1.5} className="card-price">
-                                    {
-                                        item.price.toString().length >= 3 ? item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : item.price
-                                    } ₽
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    )
-                }))
+                                    <CardContent className="card-content">
+                                        <Typography gutterBottom component="div" className="card-title" fontSize={16}>
+                                            {item.title}
+                                        </Typography>
+                                        <Typography>
+                                            <Rating 
+                                                name="read-only" 
+                                                value={item.rating}
+                                                precision={0.5}
+                                                icon={<img src={star} style={{marginRight: "4px"}} alt="Иконка золотой звезды" />}
+                                                emptyIcon={<img src={emptyStar} style={{marginRight: "4px"}} alt="Иконка белой звезды" />}
+                                                readOnly 
+                                            />
+                                        </Typography>
+                                        <Typography fontWeight="bold" mt={1.5} className="card-price">
+                                            {
+                                                item.price.toString().length >= 3 ? item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : item.price
+                                            } ₽
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
             }
-            <Pagination count={10} page={page} onChange={handlePageChange} />
+            <Pagination 
+                count={10} 
+                page={page} 
+                onChange={handlePageChange} 
+                variant="outlined" 
+                shape="rounded"
+                boundaryCount={2}
+                siblingCount={0}
+            />
         </main>
     );
 }
